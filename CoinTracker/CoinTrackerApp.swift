61b9ec05b255a800cf1de7ecdba56f858e28dffe
Joshua Cleetus <joshua.cleetus@gmail.com>
2022-01-08 17:00:52 -0500

    Initial Commit

---
+++ CoinTracker/CoinTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinTrackerApp.swift
+//  CoinTracker
+//
+//  Created by Joshua Cleetus on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CoinTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
